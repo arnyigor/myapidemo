80d5dd5ac22bb49ecf379ef105baf681347ca553
i.sedoy <i.sedoy@aristos.pw>
2017-06-15 18:17:14 +0300

    asynctask

--- app/src/main/java/com/arny/myapidemo/activities/LoaderActivity.java
+++ app/src/main/java/com/arny/myapidemo/activities/LoaderActivity.java
@@ -2,19 +2,20 @@ package com.arny.myapidemo.activities;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import com.arny.arnylib.loaders.ITaskLoaderListener;
-import com.arny.arnylib.loaders.SampleTask;
+import com.arny.arnylib.loaders.RotationWeakAsyncTask;
+import com.arny.arnylib.network.NetworkService;
+import com.arny.arnylib.network.OnJSONRequestResult;
 import com.arny.myapidemo.R;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class LoaderActivity extends AppCompatActivity implements ITaskLoaderListener {
+public class LoaderActivity extends AppCompatActivity {
 	private static final String TAG = "LoaderActivity";
-	private static final int TASK_ID = 1;
 	private Context context;
 
 	@Override
@@ -33,19 +34,41 @@ public class LoaderActivity extends AppCompatActivity implements ITaskLoaderList
         
     }
 
+
 	protected void doShowProgress() {
-		SampleTask.execute(this, this);
+		RotationWeakAsyncTask.startNewTask(new RotationWeakAsyncTask(this, "res") {
+			public void run() {
+				SystemClock.sleep(10000);
+				NetworkService.apiRequest(LoaderActivity.this, "http://dev.aristos.pw:5055?id=test&timestamp=1497539544&lat=55.68143089&lon=37.58964666&speed=0.0&bearing=0.0&altitude=210.0&batt=100.0&emei=353498080599854", new JSONObject(), new OnJSONRequestResult() {
+					@Override
+					public void onResult(JSONObject object) {
+						Log.i(LoaderActivity.class.getSimpleName(), "onResult: object = " + object);
+					}
+
+					@Override
+					public void onError(String error) {
+						Log.i(LoaderActivity.class.getSimpleName(), "onError: error = " + error);
+					}
+				});
+			}
+		}, new RotationWeakAsyncTask.OnTaskFinishedListener() {
+			@Override
+			public void onTaskFinished(int i) {
+				Log.i(LoaderActivity.class.getSimpleName(), "onTaskFinished: i = " + i);
+			}
+		}, 0);
 	}
 
 	@Override
-	public void onCancelLoad() {
-		Log.d(TAG, "task canceled");
+	protected void onResume() {
+		super.onResume();
+		RotationWeakAsyncTask.setContext(this, null);
 	}
 
-	@Override
-	public void onLoadFinished(Object data) {
-		if(data!=null && data instanceof String){
-			Log.d(TAG, "task result: " + data);
-		}
+	public void onPause() {
+		super.onPause();
+		RotationWeakAsyncTask.resetContext();
 	}
+
+
 }
