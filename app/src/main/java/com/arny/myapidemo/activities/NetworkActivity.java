2b7818e10db6aa7fe87067bd0e8a0c0fd498769c
Arny <arnyigor@gmail.com>
2017-06-26 23:33:00 +0300

    network

--- app/src/main/java/com/arny/myapidemo/activities/NetworkActivity.java
+++ app/src/main/java/com/arny/myapidemo/activities/NetworkActivity.java
@@ -4,11 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.arny.arnylib.network.NetworkService;
+import com.arny.arnylib.network.OnJSONArrayResult;
 import com.arny.myapidemo.R;
 import com.arny.myapidemo.services.Operations;
+import org.json.JSONArray;
 
 public class NetworkActivity extends AppCompatActivity  {
     Button stringLoaderBtn,jsonLoaderBtn;
@@ -27,7 +35,17 @@ public class NetworkActivity extends AppCompatActivity  {
         stringLoaderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Operations.onStartOperation(intent,NetworkActivity.this,Operations.EXTRA_KEY_TYPE_SYNC,2,null);
+	            NetworkService.apiGetRequest("http://beta.json-generator.com/api/json/get/4J2sIi6Tf", "json", new OnJSONArrayResult() {
+		            @Override
+		            public void onSuccess(JSONArray array) {
+			            Log.i(NetworkActivity.class.getSimpleName(), "onSuccess: array = " + array);
+		            }
+
+		            @Override
+		            public void onError(String error) {
+			            Log.i(NetworkActivity.class.getSimpleName(), "onError: error = " + error);
+		            }
+	            });
             }
         });
         jsonLoaderBtn = (Button) findViewById(R.id.buttonJSONRequest);
