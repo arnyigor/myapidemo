0ebc4f2430e109b1c2c3e0db8983c6eafd9276b1
i.sedoy <i.sedoy@aristos.pw>
2017-09-14 11:04:16 +0300

    fix

--- app/src/main/java/com/arny/myapidemo/models/TestObject.java
+++ app/src/main/java/com/arny/myapidemo/models/TestObject.java
@@ -1,9 +1,6 @@
 package com.arny.myapidemo.models;
 
-import android.database.Cursor;
 import com.arny.arnylib.database.DBProvider;
-import com.arny.arnylib.utils.Utility;
-import org.chalup.microorm.MicroOrm;
 import org.chalup.microorm.annotations.Column;
 
 import java.io.Serializable;
@@ -11,40 +8,17 @@ import java.util.ArrayList;
 
 
 public class TestObject implements Serializable {
-	@Column("id")
-	private String id;
-    private long dbID;
-	@Column("title")
+    @Column("id")
+    private String id;
+    private long dbId;
+    @Column("title")
     private String name;
+
     private ArrayList<GoodItem> goodItems;
 
-	public TestObject() {
-	}
-
-	public TestObject(String id,String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public long getDbID() {
-        return dbID;
-    }
-
-    public void setDbID(long dbID) {
-        this.dbID = dbID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+	public TestObject(String id, String name) {
+        this.id = id;
         this.name = name;
-    }
-
-    public TestObject getFromCursor(Cursor c){
-	    MicroOrm uOrm = new MicroOrm();
-	    return uOrm.fromCursor(c, this.getClass());
     }
 
     @Override
@@ -52,19 +26,19 @@ public class TestObject implements Serializable {
         return DBProvider.getColumns(this);
     }
 
-    public ArrayList<GoodItem> getGoodItems() {
-        return goodItems;
+    public String getId() {
+        return id;
     }
 
-    public void setGoodItems(ArrayList<GoodItem> goodItems) {
-        this.goodItems = goodItems;
+    public void setId(String id) {
+        this.id = id;
     }
 
-	public String getId() {
-		return id;
-	}
+    public long getDbId() {
+        return dbId;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setDbId(long dbId) {
+        this.dbId = dbId;
+    }
 }
