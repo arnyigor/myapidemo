51fb2f50d023da11a782ef7637770bbeb6392a9c
arny <arnyigor@gmail.com>
2017-12-24 22:35:13 +0300

    rx

--- app/src/main/java/com/arny/myapidemo/ui/activities/RxJavaActivity.java
+++ app/src/main/java/com/arny/myapidemo/ui/activities/RxJavaActivity.java
@@ -18,8 +18,11 @@ import com.arny.myapidemo.api.Auth;
 import com.arny.myapidemo.models.GoodItem;
 import com.arny.myapidemo.utils.Local;
 import com.jakewharton.rxbinding2.widget.RxTextView;
-import io.reactivex.Observable;
+import io.reactivex.*;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.BiConsumer;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import org.json.JSONObject;
 
@@ -27,226 +30,264 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 public class RxJavaActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText edt;
-    private Stopwatch stopwatch;
-    private Observable<ArrayList<GoodItem>> obs = rx2();
-    private Button btn;
-    private AristorService aristos;
-    private ProgressDialog pDialog;
+	private EditText edt;
+	private Stopwatch stopwatch;
+	private Observable<ArrayList<GoodItem>> obs = rx2();
+	private Button btn;
+	private AristorService aristos;
+	private ProgressDialog pDialog;
+	private Button btnTest1;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rx_java);
-        btn = (Button) findViewById(R.id.btn_login);
-        btn.setOnClickListener(this);
-        edt = (EditText) findViewById(R.id.editText);
-        findViewById(R.id.btnTest).setOnClickListener(this);
-        pDialog = new ProgressDialog(this);
-        pDialog.setCancelable(false);
-	    findViewById(R.id.btnGetTransfers).setOnClickListener(this);
-        aristos = ApiFactory.getInstance().createService(AristorService.class, API.BASE_URL_ARISTOS);
-        getRxEdit()
-                .map(s -> s.length() >= 3)
-                .subscribe(res -> btn.setEnabled(res));
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_rx_java);
+		btn = findViewById(R.id.btn_login);
+		btnTest1 = findViewById(R.id.btnTest1);
+		btn.setOnClickListener(this);
+		btnTest1.setOnClickListener(this);
+		edt = findViewById(R.id.editText);
+		findViewById(R.id.btnTest).setOnClickListener(this);
+		pDialog = new ProgressDialog(this);
+		pDialog.setCancelable(false);
+		findViewById(R.id.btnGetTransfers).setOnClickListener(this);
+		aristos = ApiFactory.getInstance().createService(AristorService.class, API.BASE_URL_ARISTOS);
+		getRxEdit()
+				.map(s -> s.length() >= 3)
+				.subscribe(res -> btn.setEnabled(res));
+	}
 
-    private Observable<String> getRxEdit() {
-        return RxTextView.textChanges(edt)
-                .map(CharSequence::toString).skip(1)
-                .doOnNext(s -> {
-                    boolean shortPass = s.length() < 3;
-                    btn.setEnabled(!shortPass);
-                    if (shortPass) {
-                        runOnUiThread(() -> {
-                            btn.setEnabled(false);
-                            edt.setError("Короткий пароль");
-                        });
-                    }
-                })
-                .debounce(1, TimeUnit.SECONDS)
-                .doOnNext(System.out::println)
-                .onErrorReturn(throwable -> null)
-                .subscribeOn(Schedulers.io())//где обрабатывать(бг поток)
-                .observeOn(AndroidSchedulers.mainThread());//куда возвращать(главный)
-    }
+	private Observable<String> getRxEdit() {
+		return RxTextView.textChanges(edt)
+				.map(CharSequence::toString).skip(1)
+				.doOnNext(s -> {
+					boolean shortPass = s.length() < 3;
+					btn.setEnabled(!shortPass);
+					if (shortPass) {
+						runOnUiThread(() -> {
+							btn.setEnabled(false);
+							edt.setError("Короткий пароль");
+						});
+					}
+				})
+				.debounce(1, TimeUnit.SECONDS)
+				.doOnNext(System.out::println)
+				.onErrorReturn(throwable -> null)
+				.subscribeOn(Schedulers.io())//где обрабатывать(бг поток)
+				.observeOn(AndroidSchedulers.mainThread());//куда возвращать(главный)
+	}
 
-    private List<Integer> generateList() {
-        List<Integer> integers = new ArrayList<>();
-        int rand = MathUtils.randInt(50, 200);
-        System.out.println("Generating integers\nrand:" + rand);
-        for (int i = 0; i < 100; i++) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                return integers;
-            }
-            System.out.println(Thread.currentThread().getName() + " thread " + " iteration:" + i + " " + stopwatch.getElapsedTimeSecs(3) + " sec");
-            if (rand == i) {
-                try {
-                    throw new Exception("rand is found:" + i);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return integers;
-                }
-            }
-            integers.add(i);
-        }
-        return integers;
-    }
+	private List<Integer> generateList() {
+		List<Integer> integers = new ArrayList<>();
+		int rand = MathUtils.randInt(50, 200);
+		System.out.println("Generating integers\nrand:" + rand);
+		for (int i = 0; i < 100; i++) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return integers;
+			}
+			System.out.println(Utility.getThread() + " thread " + " iteration:" + i + " " + stopwatch.getElapsedTimeSecs(3) + " sec");
+			if (rand == i) {
+				try {
+					throw new Exception("rand is found:" + i);
+				} catch (Exception e) {
+					e.printStackTrace();
+					return integers;
+				}
+			}
+			integers.add(i);
+		}
+		return integers;
+	}
 
-    private boolean savetest(List<Integer> integers){
-        for (Integer integer : integers) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getName() + "thread save integer:" + integer + " time:" + stopwatch.getElapsedTimeSecs(3) + " sec");
-        }
-        return true;
-    }
+	private boolean savetest(List<Integer> integers) {
+		for (Integer integer : integers) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Utility.getThread() + "thread save integer:" + integer + " time:" + stopwatch.getElapsedTimeSecs(3) + " sec");
+		}
+		return true;
+	}
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btnTest:
-                stopwatch = new Stopwatch();
-                stopwatch.start();
-                Utility.mainThreadObservable(
-                        Observable.create(e -> {
-                            e.onNext(generateList());
-                            e.onComplete();
-                        }).map(o -> (ArrayList<Integer>)o)
-                                .map(integers -> {
-                                    ArrayList<Long> objects = new ArrayList<>(integers.size());
-                                    for (Integer integer : integers) {
-                                        objects.add(Long.valueOf(integer));
-                                    }
-                                    return objects;
-                                })
-                                .map(unsortedList -> {//сортировка списка
-                                    List<Long> sortedList = new ArrayList<>(unsortedList.size());
-                                    Collections.copy(sortedList, unsortedList);
-                                    Collections.sort(sortedList, (o1, o2) -> Long.compare(o1, o2));
-                                    return sortedList;
-                                })
-                        .map(longs -> {
-                            ArrayList<Integer> objects = new ArrayList<>(longs.size());
-                            for (Long object : longs) {
-                                objects.add(Integer.parseInt(String.valueOf(object)));
-                            }
-                            return objects;
-                        })
-                ).subscribe(integers -> {
-                    Utility.mainThreadObservable(
-                            Observable.create(e -> {
-                                e.onNext(savetest(integers));
-                                e.onComplete();
-                            }).map(o ->(Boolean) o)
-                    ).subscribe(System.out::println, Throwable::printStackTrace);
-                    System.out.println("res "+integers+" time:" + stopwatch.getElapsedTimeSecs(3) + " sec");
-                }, Throwable::printStackTrace);
-                break;
-            case R.id.btnGetTransfers:
-                API.getTransfer()
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(object -> {
-			                Log.i(RxJavaActivity.class.getSimpleName(), "onClick: object.getSuccess() = " + object.getSuccess());
-			                System.out.println(object);
-		                }, Throwable::printStackTrace);
-		        break;
-	        case R.id.btn_login:
-                aristos.login(getPostHashMap())
-                        .doOnSubscribe(disposable -> runOnUiThread(() -> DroidUtils.showProgress(pDialog, "Вход...")))
-                        .doFinally(() -> runOnUiThread(() -> DroidUtils.hideProgress(pDialog)))
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(auth -> {
-	                        System.out.println(auth);
-	                        if (auth.getSuccess()) {
-		                        if (auth.getSession() != null) {
-			                        API.setSession(auth.getSession());
-		                        }
-	                        }else{
-		                        ToastMaker.toastError(RxJavaActivity.this,auth.getError());
-	                        }
-                        },throwable -> {
-	                        ToastMaker.toastError(RxJavaActivity.this, throwable.getMessage());
-                        });
-                break;
-        }
-    }
-
-    @NonNull
-    private HashMap<String, Object> getPostHashMap() {
-        HashMap<String, Object> hashMap = new HashMap<>();
-	    JSONObject ctx = API.getAristosPostCtx();
-        hashMap.put("login", "test");
-        hashMap.put("pass", edt.getText().toString());
-        hashMap.put("context", ctx);
-        return hashMap;
-    }
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.btnTest:
+				stopwatch = new Stopwatch();
+				stopwatch.start();
+				Utility.mainThreadObservable(
+						Observable.create(e -> {
+							e.onNext(generateList());
+							e.onComplete();
+						}).map(o -> (ArrayList<Integer>) o)
+								.map(integers -> {
+									ArrayList<Long> objects = new ArrayList<>(integers.size());
+									for (Integer integer : integers) {
+										objects.add(Long.valueOf(integer));
+									}
+									return objects;
+								})
+								.map(unsortedList -> {//сортировка списка
+									List<Long> sortedList = new ArrayList<>(unsortedList.size());
+									Collections.copy(sortedList, unsortedList);
+									Collections.sort(sortedList, (o1, o2) -> Long.compare(o1, o2));
+									return sortedList;
+								})
+								.map(longs -> {
+									ArrayList<Integer> objects = new ArrayList<>(longs.size());
+									for (Long object : longs) {
+										objects.add(Integer.parseInt(String.valueOf(object)));
+									}
+									return objects;
+								})
+				).subscribe(integers -> {
+					Utility.mainThreadObservable(
+							Observable.create(e -> {
+								e.onNext(savetest(integers));
+								e.onComplete();
+							}).map(o -> (Boolean) o)
+					).subscribe(System.out::println, Throwable::printStackTrace);
+					System.out.println("res " + integers + " time:" + stopwatch.getElapsedTimeSecs(3) + " sec");
+				}, Throwable::printStackTrace);
+				break;
+			case R.id.btnGetTransfers:
+				API.getTransfer()
+						.subscribeOn(Schedulers.io())
+						.observeOn(AndroidSchedulers.mainThread())
+						.subscribe(object -> {
+							Log.i(RxJavaActivity.class.getSimpleName(), "onClick: object.getSuccess() = " + object.getSuccess());
+							System.out.println(object);
+						}, Throwable::printStackTrace);
+				break;
+			case R.id.btn_login:
+				aristos.login(getPostHashMap())
+						.doOnSubscribe(disposable -> runOnUiThread(() -> DroidUtils.showProgress(pDialog, "Вход...")))
+						.doFinally(() -> runOnUiThread(() -> DroidUtils.hideProgress(pDialog)))
+						.subscribeOn(Schedulers.io())
+						.observeOn(AndroidSchedulers.mainThread())
+						.subscribe(auth -> {
+							System.out.println(auth);
+							if (auth.getSuccess()) {
+								if (auth.getSession() != null) {
+									API.setSession(auth.getSession());
+								}
+							} else {
+								ToastMaker.toastError(RxJavaActivity.this, auth.getError());
+							}
+						}, throwable -> {
+							ToastMaker.toastError(RxJavaActivity.this, throwable.getMessage());
+						});
+				break;
+			case R.id.btnTest1:
+				Stopwatch stopwatch = new Stopwatch();
+				stopwatch.start();
+				Observable<List<Integer>> buffer1 = Observable.create((ObservableOnSubscribe<Integer>) e -> {
+					for (int i = 0; i < 10; i++) {
+						Thread.sleep(1000);
+						Log.i(RxJavaActivity.class.getSimpleName(), "Observable.create 1:  = " + i + " time:" + stopwatch.getElapsedTimeSecs(3) + "sec");
+						e.onNext(i);
+					}
+					e.onComplete();
+				}).buffer(5);
+				Observable<List<Integer>> b2 = Observable.range(0, 10).doOnNext(integer -> {
+					Thread.sleep(300);
+					Log.i(RxJavaActivity.class.getSimpleName(), "Observable.create 2:  = " + integer + " time:" + stopwatch.getElapsedTimeSecs(3) + "sec");
+				}).buffer(5);
+				Utility.mainThreadObservable(Observable.zip(buffer1, b2, (integers, integers2) -> {
+					StringBuilder res = new StringBuilder();
+					res.append("zip 1:{");
+					for (Integer integer : integers) {
+						res.append(integer);
+						res.append(",");
+					}
+					res.append("}");
+					res.append("zip 2:{");
+					for (Integer integer : integers2) {
+						res.append(integer);
+						res.append(",");
+					}
+					res.append("}");
+					return res.toString();
+				})).subscribe(
+						s -> Log.i(RxJavaActivity.class.getSimpleName(), "zip: res = " + s + " time res:" + stopwatch.getElapsedTimeSecs(3) + "sec"), Throwable::printStackTrace
+				);
+				break;
+		}
+	}
 
 	@NonNull
-    private HashMap<String, String> getAuthSession(Auth auth) {
-        String session = auth.getSession();
-        HashMap<String, String> map = new HashMap<>();
-        map.put("session1", session);
-        return map;
-    }
+	private HashMap<String, Object> getPostHashMap() {
+		HashMap<String, Object> hashMap = new HashMap<>();
+		JSONObject ctx = API.getAristosPostCtx();
+		hashMap.put("login", "test");
+		hashMap.put("pass", edt.getText().toString());
+		hashMap.put("context", ctx);
+		return hashMap;
+	}
 
-    private void rx1() {
-        Observable.just(1, 2, 4, 8, 16, 32, 64)
-                .filter(integer -> integer > 11)
-                .map(String::valueOf)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(integer -> Log.i(RxJavaActivity.class.getSimpleName(), integer));
-    }
+	@NonNull
+	private HashMap<String, String> getAuthSession(Auth auth) {
+		String session = auth.getSession();
+		HashMap<String, String> map = new HashMap<>();
+		map.put("session1", session);
+		return map;
+	}
 
-    private String getTimeString(String s) {
-        if (s.length() >= 3) {
-            return s.substring(0, 2) + ":" + s.substring(2, s.length());
-        }
-        return s;
-    }
+	private void rx1() {
+		Observable.just(1, 2, 4, 8, 16, 32, 64)
+				.filter(integer -> integer > 11)
+				.map(String::valueOf)
+				.subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe(integer -> Log.i(RxJavaActivity.class.getSimpleName(), integer));
+	}
 
-    private Observable<ArrayList<GoodItem>> rx2() {
-        stopwatch = new Stopwatch();
-        stopwatch.start();
-        return Observable.create(e -> {
-            ArrayList<GoodItem> list = Local.generateItems(10, 20, 0);
-            e.onNext(list);
-            e.onComplete();
-        })
-                .onErrorReturn(throwable -> new ArrayList<GoodItem>())//при ошибке
-                .map(o -> (ArrayList<GoodItem>) o)//преобразуем
-                .map(goodItems -> {
-                    for (GoodItem goodItem : goodItems) {
-                        goodItem.setImage(Generator.getImageUrl(150, 150, Generator.GENERATOR_TYPE_IMG_PLACEIMG));
-                    }
-                    return goodItems;
-                })//преобразуем
-                .doOnSubscribe(disposable -> {
-                    System.out.println("doOnSubscribe:" + stopwatch.getElapsedTimeMili() + " ms");
-                })//действие при подписке
-                .subscribeOn(Schedulers.io())//где обрабатывать(бг поток)
-                .observeOn(AndroidSchedulers.mainThread())//куда возвращать(главный)
-                .doFinally(() -> {
-                    System.out.println("doFinally:" + stopwatch.getElapsedTimeMili() + " ms");
-                    stopwatch.stop();
-                });//делаем при финале
-    }
+	private String getTimeString(String s) {
+		if (s.length() >= 3) {
+			return s.substring(0, 2) + ":" + s.substring(2, s.length());
+		}
+		return s;
+	}
 
-    private void work(ArrayList<GoodItem> goodItems) {
-        System.out.println("work:" + stopwatch.getElapsedTimeMili() + " ms");
-        Log.i(RxJavaActivity.class.getSimpleName(), "work: goodItems = " + goodItems);
-    }
+	private Observable<ArrayList<GoodItem>> rx2() {
+		stopwatch = new Stopwatch();
+		stopwatch.start();
+		return Observable.create(e -> {
+			ArrayList<GoodItem> list = Local.generateItems(10, 20, 0);
+			e.onNext(list);
+			e.onComplete();
+		})
+				.onErrorReturn(throwable -> new ArrayList<GoodItem>())//при ошибке
+				.map(o -> (ArrayList<GoodItem>) o)//преобразуем
+				.map(goodItems -> {
+					for (GoodItem goodItem : goodItems) {
+						goodItem.setImage(Generator.getImageUrl(150, 150, Generator.GENERATOR_TYPE_IMG_PLACEIMG));
+					}
+					return goodItems;
+				})//преобразуем
+				.doOnSubscribe(disposable -> {
+					System.out.println("doOnSubscribe:" + stopwatch.getElapsedTimeMili() + " ms");
+				})//действие при подписке
+				.subscribeOn(Schedulers.io())//где обрабатывать(бг поток)
+				.observeOn(AndroidSchedulers.mainThread())//куда возвращать(главный)
+				.doFinally(() -> {
+					System.out.println("doFinally:" + stopwatch.getElapsedTimeMili() + " ms");
+					stopwatch.stop();
+				});//делаем при финале
+	}
+
+	private void work(ArrayList<GoodItem> goodItems) {
+		System.out.println("work:" + stopwatch.getElapsedTimeMili() + " ms");
+		Log.i(RxJavaActivity.class.getSimpleName(), "work: goodItems = " + goodItems);
+	}
 
 }
